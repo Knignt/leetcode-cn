77e85f432bf2a3682cc28251c3c915e412a74c74
Xuan <854674282@qq.com>
2023-08-30 23:26:30 +0800

    Create 34.php

---
+++ 34.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ *给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+
+
+示例 1：
+
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+
+
+提示：
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums 是一个非递减数组
+-109 <= target <= 109
+ *
+ * */
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function searchRange($nums, $target) {
+        if(empty($nums)) return [-1,-1];
+
+        $res = [];
+        for($i=0;$i<count($nums);$i++){
+            if($nums[$i] > $target) break;
+            if($nums[$i] == $target){
+                $res[] = $i;
+            }
+        }
+        return empty($res) ? [-1,-1] : [$res[0], $res[count($res)-1]];
+    }
+}
+
+$a = new Solution();
+$nums = [5,7,7,8,8,10];
+$target = 8;
+var_dump($a->searchRange($nums, $target));
